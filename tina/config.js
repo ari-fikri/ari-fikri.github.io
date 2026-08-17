5b2a3dd6e7a10a0ac60c0cb8a66a4c96eed6605e
Ari Fikri <arirushan79@gmail.com>
2026-08-17 11:15:08 +0700

    update indexer token

--- tina/config.js
+++ tina/config.js
@@ -111,7 +111,7 @@ export default defineConfig({
   },
   search: {
     tina: {
-      indexerToken: process.env.TINA_SEARCH_TOKEN,
+      indexerToken: process.env.NEXT_PUBLIC_TINA_SEARCH_TOKEN,
       stopwordLanguages: ['eng'],
     },
     indexBatchSize: 100,
